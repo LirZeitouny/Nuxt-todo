25bd6dafa67f75371486930c85dd7d75acae3694
Lir Zeitouny <lirz@wix.com>
2022-02-17 14:11:54 +0200

    empty

---
+++ pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>My blog navigation bar here</div>
+    <Nuxt />
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: "Hello world!",
+    };
+  },
+};
+</script>
+
+<style>
+.example {
+  margin-left: 10em;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <home />
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Home from "./Home.vue";
+
+export default Vue.extend({
+  components: { Home },
+  name: "IndexPage",
+});
+</script>
